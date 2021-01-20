e373f88bbd26c727aaba6a859badbd3e7514845b
Olivier <bourgogne.olivier@gmail.com>
2021-01-20 13:39:03 +0100

    final

---
+++ controller/shop_controller.php
@@ -0,0 +1,16 @@
+<?php
+
+include '../model/user.php';
+
+//  if (isset($_POST['add'])) {
+//           var_dump(getItem($pdo, $_POST['add']));
+// }
+
+// echo "valeur".$_POST["add"];
+
+// var_dump($_POST['add']);
+
+
+include '../view/shop_view.php';  
+
+
---
+++ view/shop_view.php
@@ -0,0 +1,58 @@
+<form action="shop" method= "post" >
+    <div class="product-details">
+        <img src=
+        "https://www.valoisvintage-paris.com/13932-81315-large_default/serviette-de-plage-chanel-bleu-marine-logo-cc.jpg" 
+        class="pics" 
+        alt="serviette" />
+        <p class="legend">La Serviette</p>
+        <p class="price">Prix: 20 euros</p>
+        <button type="submit" name="add" value="6">Ajouter au panier</button>
+    </div>
+        <br>
+    <div class="product">
+        <img src=
+        "https://cdn.laredoute.com/products/641by641/0/9/f/09f5691723cab9d70c8882947a1ad027.jpg" 
+        class="pics" 
+        alt="futa" />
+        <p class="legend">La Futa</p>
+        <p class="price">Prix: 30 euros</p>
+        <button type="submit"  name="add" value="7">Ajouter au panier</button>
+    </div>
+        <br>
+    <div class="product">
+        <img src="https://obsessive.fr/media/catalog/product/cache/5/image/0dc2d03fe217f8c83829496872af24a0/9/0/90be8419971ebd969d4c371c6caaa0dd.jpg" 
+        class="pics" 
+        alt="peignoir" />
+        <p class="legend">Le Peignoir</p>
+        <p class="price">Prix: 40 euros</p>
+        <button type="submit"  name="add" value="8">Ajouter au panier</button>
+    </div>
+        <br>
+    <div class="product">
+        <img src=
+        "https://static1.purebreak.com/articles/5/19/31/95/@/712087-vin-diesel-pourquoi-ses-personnages-po-diapo-2.jpg" 
+        class="pics" 
+        alt="mrcl" />
+        <p class="legend">Le Marcel</p>
+        <p class="price">Prix: 15 euros</p>
+        <button type="submit"  name="add" value="9">Ajouter au panier</button>
+    </div>
+        <br>
+    <div class="product">
+        <img src=
+        "https://www.modekinko.com/15938-large_m/maillot-de-bain-blanc-pas-cher-bikini-maillot-de-bain-2-pieces-pas-cher.jpg" 
+        class="pics" 
+        alt="mdb" />
+        <p class="legend">Le Maillot de bain</p>
+        <p class="price">Prix: 80 euros</p>
+        <button type="submit"  name="add" value="10">Ajouter au panier</button>
+    </div>
+</div>
+
+<style>
+.pics {
+width: 150px;
+border-radius: 50%;
+}
+</style>
+          
